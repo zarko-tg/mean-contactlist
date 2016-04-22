3de6b35d71b8dda5078ec10279e3fe58d43d68ea
Zarko Hristovski <zarko@timegrip.no>
2016-04-22 11:55:47 +0200

    Test

--- server.js
+++ server.js
@@ -2,9 +2,76 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
+var crypto = require('crypto');
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
+
+var ALGORITHM, KEY, HMAC_ALGORITHM, HMAC_KEY;
+ALGORITHM = 'AES-256-CTR';
+HMAC_ALGORITHM = 'SHA512';
+KEY = crypto.randomBytes(32); // This key should be stored in an environment variable
+HMAC_KEY = crypto.randomBytes(32); // This key should be stored in an environment variable
+
+var encrypt = function (plain_text) {
+
+    var IV = new Buffer(crypto.randomBytes(16)); // ensure that the IV (initialization vector) is random
+    var cipher_text;
+    var hmac;
+    var encryptor;
+
+    encryptor = crypto.createCipheriv(ALGORITHM, KEY, IV);
+    encryptor.setEncoding('hex');
+    encryptor.write(plain_text);
+    encryptor.end();
+
+    cipher_text = encryptor.read();
+
+    hmac = crypto.createHmac(HMAC_ALGORITHM, HMAC_KEY);
+    hmac.update(cipher_text);
+    hmac.update(IV.toString('hex')); // ensure that both the IV and the cipher-text is protected by the HMAC
+
+    // The IV isn't a secret so it can be stored along side everything else
+    return cipher_text + "$" + IV.toString('hex') + "$" + hmac.digest('hex');
+};
+
+var decrypt = function (cipher_text) {
+    var cipher_blob = cipher_text.split("$");
+    var ct = cipher_blob[0];
+    var IV = new Buffer(cipher_blob[1], 'hex');
+    var hmac = cipher_blob[2];
+    var decryptor, chmac, decryptedText;
+
+    chmac = crypto.createHmac(HMAC_ALGORITHM, HMAC_KEY);
+    chmac.update(ct);
+    chmac.update(IV.toString('hex'));
+
+    if (!constant_time_compare(chmac.digest('hex'), hmac)) {
+        console.log("Encrypted Blob has been tampered with...");
+        return null;
+    }
+
+    decryptor = crypto.createDecipheriv(ALGORITHM, KEY, IV);
+    decryptedText = decryptor.update(ct, 'hex', 'utf-8');
+    return decryptedText + decryptor.final('utf-8');
+};
+
+var constant_time_compare = function (val1, val2) {
+    var sentinel;
+
+    if (val1.length !== val2.length) {
+        return false;
+    }
+
+
+    for (var i = 0; i <= (val1.length - 1); i++) {
+        sentinel |= val1.charCodeAt(i) ^ val2.charCodeAt(i);
+    }
+
+    return sentinel === 0
+};
+
+
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
@@ -109,3 +176,8 @@ app.delete("/contacts/:id", function(req, res) {
     }
   });
 });
+
+var blob = encrypt("FOO");
+console.log(blob);
+var unblob = decrypt(blob);
+console.log(unblob);
